80559ac8393b73ffbc2e9352fca9f3606fd09322
Justwouter <wouter2312@gmail.com>
2023-06-16 17:38:36 +0200

    Added soundeffect for win

--- src/main/java/com/gui/WinnerController.java
+++ src/main/java/com/gui/WinnerController.java
@@ -1,5 +1,6 @@
 package com.gui;
 
+import java.io.File;
 import java.util.Map;
 
 import javafx.fxml.FXML;
@@ -7,6 +8,8 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class WinnerController extends AController{
 
@@ -19,7 +22,12 @@ public class WinnerController extends AController{
     @FXML
     private BorderPane MainPane;
 
+    private MediaPlayer musicPlayer;
+
+
+
     public void switchToGame() throws Exception{
+        musicPlayer.dispose();
         Main.show("homepage",this.dataDict);
     }
     
@@ -27,9 +35,18 @@ public class WinnerController extends AController{
     public void loadData(Map<String, Object> dataDict) {
         this.dataDict = dataDict;
 
-
         Label winLabel = new Label((String) dataDict.get("VictoryText"));
         MainPane.setTop(winLabel);
         BorderPane.setAlignment(winLabel, Pos.CENTER);
+        this.musicPlayer = startMusic(Main.class.getResource("music/YAY.mp3").getFile());
     }
+
+    private MediaPlayer startMusic(String pathToMusic){
+        File file = new File(pathToMusic);
+        Media media = new Media(file.toURI().toString()); 
+        MediaPlayer musicPlayer = new MediaPlayer(media);
+        musicPlayer.setAutoPlay(true);
+        return musicPlayer;
+    }
+
 }
